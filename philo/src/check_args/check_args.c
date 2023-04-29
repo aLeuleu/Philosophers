a573f2070e4d6ad8bb25a3493d21773114b65b55
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-04-29 17:24:15 +0200

    [KO]structure project

--- philo/src/check_args/check_args.c
+++ philo/src/check_args/check_args.c
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/24 16:34:13 by alevra            #+#    #+#             */
-/*   Updated: 2023/02/24 16:35:43 by alevra           ###   ########.fr       */
+/*   Created: 2023/02/24 16:12:12 by alevra            #+#    #+#             */
+/*   Updated: 2023/02/24 16:45:28 by alevra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#if !defined(PHILO_H)
-#define PHILO_H
-# define VALID_ARGS 1
-# define INVALID_ARGS -1
+#include "philo.h"
 
-struct	s_philo
+int	check_args(int argc, char **argv)
 {
-	int number_of_philosophers ;
-	int time_to_die ;
-	int time_to_eat ;
-	int time_to_sleep ;
-	int number_of_times_each_philosopher_must_eat ;
-	
-} typedef t_philo;
-
-int	check_args(int argc, char **argv);
-
-#endif // PHILO_H
+	if (check_args_count(argc) && check_args_overflows(argv))
+		return (VALID_ARGS);
+	return (INVALID_ARGS);
+}
--- philo/src/main.c
+++ philo/src/main.c
@@ -12,33 +12,16 @@
 
 #include "philo.h"
 
-static int	check_args(int argc, char **argv);
-
 int	main(int argc, char **argv)
 {
 	t_philo	*philosophers = 0;
 
 	(void)philosophers;
-	check_args(argc, argv);
+	if (check_args(argc, argv) != VALID_ARGS)
+		return (error_msg(INVALID_ARGS));
 //	create_philosophers(philosophers, argv);
 //	create_monitor();
 //	run_philosophers(philosophers);
 //	return(clean_up(philosophers));
-	return (0);
-}
-
-static int	check_args(int argc, char **argv)
-{
-
-	if (argc < 5 || argc > 6)
-	{
-//		ft_putstr_fd("Bad number of arguments:\n", STDERR_FILENO);
-//		ft_putstr_fd("\t./philo number_of_philosophers time_to_die ",
-//					 STDERR_FILENO);
-//		ft_putstr_fd("time_to_eat time_to_sleep ", STDERR_FILENO);
-//		ft_putstr_fd("[number_of_times_each_philosopher_must_eat]\n",
-//					 STDERR_FILENO);
-		return (-1);
-	}
 	return (0);
 }
